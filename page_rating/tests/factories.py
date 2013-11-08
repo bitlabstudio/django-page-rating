587367c8505daaf2b18eeec6fc854f3599329a79
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-11-08 09:22:00 +0100

    Added fail safe importing of Factory.
    After adding django-libs and installing it, factory-boy was downgraded to
    version 1.3.0. This ensures, that the tests work with either that or v2.0.0+

--- page_rating/tests/factories.py
+++ page_rating/tests/factories.py
@@ -1,17 +1,24 @@
 """Factories for the page_rating app."""
+# so we got conflicting factory-boy version requirements. I'd rather use the
+# 2.0.0+ version here, but django-libs requires factory-boy<2.0.0 resulting in
+# v1.3.0
 import factory
+try:
+    from factory.django import DjangoModelFactory as Factory
+except ImportError:
+    from factory import Factory
 
 from ..models import Rating, UserRating
 
 
-class RatingFactory(factory.django.DjangoModelFactory):
+class RatingFactory(Factory):
     """Factory for the ``Rating`` model."""
     FACTORY_FOR = Rating
 
     page_url = factory.Sequence(lambda n: '/page_url/{0}/'.format(n))
 
 
-class UserRatingFactory(factory.django.DjangoModelFactory):
+class UserRatingFactory(Factory):
     """Factory for the ``UserRating`` model."""
     FACTORY_FOR = UserRating
 
